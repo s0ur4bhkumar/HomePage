3019ab61702863d9fb6e0da7dc72e666f2783402
s0ur4bhkumar <soul65917@gmail.com>
2026-03-07 15:20:52 +0530

    complete desktop layout except animations

--- configs/webpack.dev.js
+++ configs/webpack.dev.js
@@ -1,9 +1,10 @@
+import path from "node:path";
 export default {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
     hot: true,
     port: 3000,
-    watchFiles: "../src/template.html",
+    watchFiles: path.resolve(import.meta.dirname, "../src/template.html"),
   },
 };
